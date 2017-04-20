d1f1811d85ccd3c69b306cd79e03d2b26ee1ade6
Brett Jacobitz <brettjacobitz@gmail.com>
2017-04-20 02:17:42 -0500

    Added base target attribute

--- Lawnhiro/PayPalTest.aspx.cs
+++ Lawnhiro/PayPalTest.aspx.cs
@@ -9,6 +9,13 @@ namespace Lawnhiro
 {
     public partial class PayPalTest : System.Web.UI.Page
     {
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (!Request.IsSecureConnection && !Request.Url.AbsoluteUri.Contains("localhost"))
+            {
+                Response.Redirect(Request.Url.AbsoluteUri.Replace("http://", "https://"));
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
